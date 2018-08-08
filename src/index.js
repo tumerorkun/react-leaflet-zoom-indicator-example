907648c4f9dfe12cbb4c80101eb2327d73e3a0ee
Tumer <datavis001@Orkuns-MacBook-Pro.local>
2018-08-08 10:10:20 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
+import './index.css'
+import 'leaflet'
 import React from "react";
 import ReactDOM from "react-dom";
-import 'leaflet'
 import { Map, TileLayer } from 'react-leaflet'
-import { ReactLeafletZoomIndicator } from 'react-leaflet-zoom-indicator'
-import './index.css'
 
+// My Components
+import { ReactLeafletZoomIndicator } from 'react-leaflet-zoom-indicator'
+import { ReactLeafletSearch } from 'react-leaflet-search'
+
+
+const MapContainer = (props) => (
+    <Map
+        className="map"
+        scrollWheelZoom={false}
+        bounds={props.options.bounds}
+        maxZoom={props.options.maxZoom}
+        maxBounds={props.options.maxBounds}>
+        <TileLayer noWrap={true} url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {props.children}
+    </Map>
+)
+
+const SeperatorAndLinks = props => (
+    <div>
+        <hr />
+        <h2> {props.title} <small>
+            <a href={`https://github.com/tumerorkun/${props.name}`}>github</a>
+            <span> - </span>
+            <a href={`https://www.npmjs.com/package/${props.name}`}>npm</a>
+        </small> </h2>
+    </div>
+)
 
 class App extends React.Component {
     constructor(props) {
@@ -36,15 +62,29 @@ class App extends React.Component {
     render (){
         return (
             <div>
-                <Map 
-                    className="simpleMap"
-                    scrollWheelZoom={false}
-                    bounds={this.state.bounds}
-                    maxZoom={this.state.maxZoom}
-                    maxBounds={this.state.maxBounds}>
-                    <TileLayer noWrap={true} url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                <h1> React Leaflet Component Examples </h1>
+                <br />
+                <h2> All components together </h2>
+                <MapContainer options={this.state}>
                     <ReactLeafletZoomIndicator head='zoom:' position='topleft' />
-                </Map>
+                    <ReactLeafletSearch position="topleft" />
+                </MapContainer>
+
+                    <SeperatorAndLinks title={'Zoom Indicator'} name={'react-leaflet-zoom-indicator'} />
+
+                <MapContainer options={this.state}>
+                    <ReactLeafletZoomIndicator head='zoom:' position='topleft' />
+                </MapContainer>
+
+                    <SeperatorAndLinks title={'Seacrh'} name={'react-leaflet-search'} />
+
+                <MapContainer options={this.state}>
+                    <ReactLeafletSearch position="topleft" />
+                </MapContainer>
+
+                <footer>
+
+                </footer>
             </div>
         );
     }
